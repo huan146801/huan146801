a51c5fe21f060f00157b0a21ab7d013d6b3e5d56
huan146801 <huan146801@gmail.com>
2024-02-27 17:25:52 +0700

    hook

--- reactjs/tiktok/src/App.js
+++ reactjs/tiktok/src/App.js
@@ -1,32 +1,18 @@
+import Content from './Content';
 import { useState } from 'react'
 
-
 function App() {
-  const [job,setJob] = useState('');
-  const [jobs, setJobs] = useState([])
-
-  const handleSubmit = () => { 
-    setJobs(prev => [...prev, job])
-    setJob('')
-  }
-
-  console.log(job)
+  const [show, getShow] = useState(false)
 
   return (
-    <div style ={{padding: 32}}>
-      <input 
-        value ={job}
-        onChange = {e => setJob(e.target.value)}
-      />
-      <button onClick = {handleSubmit}>Add</button>
-
-      <ul>
-        {jobs.map((job,index) =>(
-          <li key={index}>{job}</li>
-        ))}
-      </ul>
+    <div style={{padding: 32}}>
+      <button onClick={()=> getShow(!show)}>Toggle</button>
+      {show && <Content />}
     </div>
-  );
+  )
+  
 }
+
+  
 
 export default App;
