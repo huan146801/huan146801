8c3e4e8ff1ec32a77f97ae8560da5f71bd230088
huan146801 <huan146801@gmail.com>
2025-01-06 11:32:59 +0700

    huan

--- react-template-main/react-template-main/src/App.tsx
+++ react-template-main/react-template-main/src/App.tsx
@@ -12,7 +12,7 @@ function App() {
           <div className="flex-1 border rounded-2xl bg-white w-[700px] ">
             <h1 className="text-black text-5xl font-bold m-5">To Do List</h1>
             <NewJob />
-            <JobList />
+            <JobList key ={Math.random()} />
           </div>
           <div className="flex-1 rounded-lg bg-[url('https://media.istockphoto.com/id/517188688/vi/anh/phong-c%E1%BA%A3nh-n%C3%BAi-non.jpg?s=612x612&w=0&k=20&c=WWWaejSo6EWGZMZSK7QK6LCfwd0rL2KB3ImCX2VkW4A=')] object-fill ">
             <img className="object-fill" />
---
+++ src/f8 web/index.js
@@ -0,0 +1,19 @@
+function showModal() {
+  document.getElementById("modal").classList.remove("hidden");
+  document.getElementById("modal").classList.add("flex");
+}
+
+function closeModal() {
+  document.getElementById("modal").classList.remove("flex");
+  document.getElementById("modal").classList.add("hidden");
+}
+
+function showModal2() {
+  document.getElementById("modal2").classList.remove("hidden");
+  document.getElementById("modal2").classList.add("flex");
+}
+
+function closeModal2() {
+  document.getElementById("modal2").classList.remove("flex");
+  document.getElementById("modal2").classList.add("hidden");
+}
